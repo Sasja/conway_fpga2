714966c2989368b796a1bbfb1f775fa77ef1a100
sasja <sasja.ws@gmail.com>
2017-05-13 20:36:26 +0200

    add blinky code and icestick pcf

---
+++ vgatest.vhdl
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity vgatest is
+  port (
+    i_clk  : in     std_logic;
+    LED1   : out    std_logic );
+end vgatest;
+
+architecture rtl of vgatest is
+
+  signal cnt : natural range 0 to 1000000;
+  signal led : std_logic;
+
+  begin
+
+    process (i_clk) is
+      begin
+        if rising_edge(i_clk) then
+          cnt <= cnt + 1;
+          if (cnt = 0) then
+            if (led = '0') then
+              led <= '1';
+            else
+              led <= '0';
+            end if;
+          end if;
+        end if;
+    end process;
+
+    LED1 <= led;
+
+end rtl;
