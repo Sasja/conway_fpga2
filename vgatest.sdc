bb1b64a97e8f6b869b5e447f67b541120f7675ee
sasja <sasja.ws@gmail.com>
2017-05-14 14:09:35 +0200

    configure PLL
    i tried to configure pll for 12Mhz -> 100.7Mhz (25.175 * 4) but i got twice
    that frequecy it appears when looking at scope. If this works i get 8 clock
    ticks per pixel!? im confused XD

--- vgatest.sdc
+++ vgatest.sdc
@@ -1,1 +1,1 @@
-create_clock -period 40.00 -name {i_clk} [get_ports {i_clk}]
+#create_clock -period 40.00 -name {i_clk} [get_ports {i_clk}]
--- vgatest.vhdl
+++ vgatest.vhdl
@@ -4,22 +4,31 @@ use ieee.numeric_std.all;
 
 entity vgatest is
   port (
-    i_clk     : in     std_logic;
+    clk12mhz  : in     std_logic;
     LED1      : out    std_logic;
     VGA_VSYNC : out    std_logic);
 end vgatest;
 
 architecture rtl of vgatest is
 
-  signal cnt : natural range 0 to 100;
-  signal led : std_logic;
+  signal cnt  : natural range 0 to 1000000;
+  signal led  : std_logic;
+  signal i_clk: std_logic;
 
   begin
+
+    vgatest_pll_inst: entity work.vgatest_pll
+    port map(
+              REFERENCECLK => clk12mhz,
+              PLLOUTCORE => open,
+              PLLOUTGLOBAL => i_clk,
+              RESET => '1'
+            );
 
     process (i_clk) is
       begin
         if rising_edge(i_clk) then
-          if (cnt = 99) then
+          if (cnt = 999999) then
             cnt <= 0;
             if (led = '0') then
               led <= '1';
